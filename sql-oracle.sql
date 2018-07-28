353a149db0ac5d84764bf9145157d0351d817c9e
ovichiro <14822344+ovichiro@users.noreply.github.com>
2018-07-28 22:07:29 +0300

    Add sql file

---
+++ sql-oracle.sql
@@ -0,0 +1,16 @@
+---- Constraint violation: child records found ---------
+
+SELECT owner, constraint_name, constraint_type, table_name, r_owner, r_constraint_name
+    FROM all_constraints
+    WHERE owner='DBTEST'AND constraint_name='FK9999AAAA8888BBBB';
+
+SELECT owner, constraint_name, constraint_type, table_name, r_owner, r_constraint_name
+    FROM all_constraints
+    WHERE owner='DBTEST'AND constraint_name='SYS_C00121212';
+      
+SELECT * FROM all_cons_columns
+  WHERE owner='DBTEST'AND constraint_name = 'SYS_C00121212';
+
+
+select * from user_cons_columns where constraint_name = 'FK9999AAAA8888BBBB';
+
